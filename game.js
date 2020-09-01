40bd6e5a8e4e22265fc58bae4eb99ec45b8614cf
atilaoliveirajr <atila.oliveira.jr@gmail.com>
2020-09-01 16:54:49 +0200

    Added question 3

--- game.js
+++ game.js
@@ -1,14 +1,20 @@
-let question1 = `Wer hat HTML erfunden?`
-let question1Answer1 = `Robbie Williams`
-let question1Answer2 = `Lady Gaga`
-let question1Answer3 = `Tim Berners-Lee`
-let question1Answer4 = `Justin Bieber`
+let question1 = `Wer hat HTML erfunden?`;
+let question1Answer1 = `Robbie Williams`;
+let question1Answer2 = `Lady Gaga`;
+let question1Answer3 = `Tim Berners-Lee`;
+let question1Answer4 = `Justin Bieber`;
 
-let question2 = `Was bedeutet das HTML tag &lta&gt?`
-let question2Answer1 = `Text Fett`
-let question2Answer2 = `Container`
-let question2Answer3 = `Ein link`
-let question2Answer4 = `Kursiv`
+let question2 = `Was bedeutet das HTML tag &lta&gt?`;
+let question2Answer1 = `Text Fett`;
+let question2Answer2 = `Container`;
+let question2Answer3 = `Ein link`;
+let question2Answer4 = `Kursiv`;
+
+let question3 = `Wie bindet man eine Website in eine Website ein?`;
+let question3Answer1 = `&lt;iframe&gt;, &lt;frame&gt, and &lt;frameset&gt`;
+let question3Answer2 = `&lt;iframe&gt;`;
+let question3Answer3 = `&lt;frame&gt;`;
+let question3Answer4 = `&lt;frameset&gt;`;
 
 let questionNumber = 0;
 let progress = 0;
@@ -33,7 +39,9 @@ function nextQuestion() {
         case 2:
             loadQuestion2();
             break;
-
+        case 3:
+            loadQuestion3();
+            break;
     }
 
 }
@@ -41,21 +49,21 @@ function nextQuestion() {
 function answer(a) {
 
     if (a == rightAnswer) {
-        document.getElementById(`correctAnswer`).classList.remove(`d-none`)
-        document.getElementById(`wrongAnswer`).classList.add(`d-none`)
+        document.getElementById(`correctAnswer`).classList.remove(`d-none`);
+        document.getElementById(`wrongAnswer`).classList.add(`d-none`);
 
         // The next question button will appear only if the user click on the right answer.
-        document.getElementById(`nextBtn`).classList.remove(`d-none`)
+        document.getElementById(`nextBtn`).classList.remove(`d-none`);
 
     } else {
-        document.getElementById(`wrongAnswer`).classList.remove(`d-none`)
-        document.getElementById(`correctAnswer`).classList.add(`d-none`)
+        document.getElementById(`wrongAnswer`).classList.remove(`d-none`);
+        document.getElementById(`correctAnswer`).classList.add(`d-none`);
     }
 }
 
 function hideElements() {
     document.getElementById(`correctAnswer`).classList.add(`d-none`);
-    document.getElementById(`nextBtn`).classList.add(`d-none`)
+    document.getElementById(`nextBtn`).classList.add(`d-none`);
 }
 
 function loadQuestion1() {
@@ -74,4 +82,13 @@ function loadQuestion2() {
     document.getElementById(`answer3`).innerHTML = question2Answer3;
     document.getElementById(`answer4`).innerHTML = question2Answer4;
     rightAnswer = 3;
+}
+
+function loadQuestion3() {
+    document.getElementById(`question`).innerHTML = question3;
+    document.getElementById(`answer1`).innerHTML = question3Answer1;
+    document.getElementById(`answer2`).innerHTML = question3Answer2;
+    document.getElementById(`answer3`).innerHTML = question3Answer3;
+    document.getElementById(`answer4`).innerHTML = question3Answer4;
+    rightAnswer = 2;
 }
